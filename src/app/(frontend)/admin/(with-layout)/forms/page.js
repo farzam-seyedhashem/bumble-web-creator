68574e1decfb98248cb4eda01897bc0767083c5c
farzamseyedhashem <farzam.seyedhashem@gmail.com>
2025-04-20 18:48:21 +0330

    f

--- src/app/(frontend)/admin/(with-layout)/forms/page.js
+++
@@ -1,9 +0,0 @@
-import FormComponent from "@admin/admin-panel/form/FormComponent";
-
-export default function FormsPage(Component) {
-	return (
-		<div className={"min-h-screen py-6"}>
-			{/*<FormComponent/>*/}
-		</div>
-	)
-}
