3c1b656744efb2190aa972e70f25ee1a6e22282f
farzamseyedhashem <farzam.seyedhashem@gmail.com>
2025-04-21 22:04:41 +0330

    fwefew

--- src/app/(frontend)/(website)/page.js
+++ src/app/(frontend)/(website)/page.js
@@ -7,75 +7,75 @@ export default async function Page() {
 	return (
 		<div>
 
-				<style>
-					{`
-                   ${JSON.parse(data.content).map((item, index) => {
-						let desktopStyles = []
-						let mobileStyles = []
-						let mobileAlignItems="flex-start"
-						let mobileJustifyContent="flex-start"
-						let mobileDisplay="block"
-						let desktopAlignItems="flex-start"
-						let desktopJustifyContent="flex-start"
-						let desktopDisplay="block"
-						if (item.styles) {
-							if (item.styles?.mobile?.display) {
-								const {
-									display: mobileDisplayx,
-									alignItems: mobileAlignItemsx,
-									justifyContent: mobileJustifyContentx,
-									...other
-								} = item.styles.mobile
-								mobileAlignItems = mobileAlignItemsx;
-								mobileJustifyContent = mobileJustifyContentx;
-								mobileDisplay = mobileDisplayx;
-								mobileStyles = other
-							}else{
+			{/*	<style>*/}
+			{/*		{`*/}
+            {/*       ${JSON.parse(data.content).map((item, index) => {*/}
+			{/*			let desktopStyles = []*/}
+			{/*			let mobileStyles = []*/}
+			{/*			let mobileAlignItems="flex-start"*/}
+			{/*			let mobileJustifyContent="flex-start"*/}
+			{/*			let mobileDisplay="block"*/}
+			{/*			let desktopAlignItems="flex-start"*/}
+			{/*			let desktopJustifyContent="flex-start"*/}
+			{/*			let desktopDisplay="block"*/}
+			{/*			if (item.styles) {*/}
+			{/*				if (item.styles?.mobile?.display) {*/}
+			{/*					const {*/}
+			{/*						display: mobileDisplayx,*/}
+			{/*						alignItems: mobileAlignItemsx,*/}
+			{/*						justifyContent: mobileJustifyContentx,*/}
+			{/*						...other*/}
+			{/*					} = item.styles.mobile*/}
+			{/*					mobileAlignItems = mobileAlignItemsx;*/}
+			{/*					mobileJustifyContent = mobileJustifyContentx;*/}
+			{/*					mobileDisplay = mobileDisplayx;*/}
+			{/*					mobileStyles = other*/}
+			{/*				}else{*/}
 
-								mobileStyles = item.styles.mobile
-							}
-							if (item.styles.desktop?.display) {
-								const {
-									display: desktopDisplayx,
-									alignItems: desktopAlignItemsx,
-									justifyContent: desktopJustifyContentx,
-									...other
-								} = item.styles.desktop
-								desktopStyles=other
-								desktopAlignItems = desktopAlignItemsx;
-								desktopJustifyContent = desktopJustifyContentx;
-								desktopDisplay = desktopDisplayx;
-							}else{
-								desktopStyles = item.styles.desktop
-							}
+			{/*					mobileStyles = item.styles.mobile*/}
+			{/*				}*/}
+			{/*				if (item.styles.desktop?.display) {*/}
+			{/*					const {*/}
+			{/*						display: desktopDisplayx,*/}
+			{/*						alignItems: desktopAlignItemsx,*/}
+			{/*						justifyContent: desktopJustifyContentx,*/}
+			{/*						...other*/}
+			{/*					} = item.styles.desktop*/}
+			{/*					desktopStyles=other*/}
+			{/*					desktopAlignItems = desktopAlignItemsx;*/}
+			{/*					desktopJustifyContent = desktopJustifyContentx;*/}
+			{/*					desktopDisplay = desktopDisplayx;*/}
+			{/*				}else{*/}
+			{/*					desktopStyles = item.styles.desktop*/}
+			{/*				}*/}
 
-							// const mobileStyles = item.styles.mobile
-							// const desktopStyles = item.styles.desktop
-							const globalStyles = item.styles.global
-							const mc = StyleToClass(desktopStyles, true, item.uniqueId)
-							const dc = StyleToClass(mobileStyles, false, item.uniqueId)
-							const gc = StyleToClass(globalStyles, false, item.uniqueId)
-							return gc + dc + mc + `.${item.uniqueId}-content{
-				align-items:${mobileAlignItems};
-				justify-content:${mobileJustifyContent};
-				display:${mobileDisplay};
-				}
-				@media only screen and (min-width: 840px) {
-				.${item.uniqueId}-content{
-				align-items:${desktopAlignItems};
-				justify-content:${desktopJustifyContent};
-				display:${desktopDisplay};
-				}
-				}`
-						}
-					}).join("")}
-				
-                `}
-				</style>
+			{/*				// const mobileStyles = item.styles.mobile*/}
+			{/*				// const desktopStyles = item.styles.desktop*/}
+			{/*				const globalStyles = item.styles.global*/}
+			{/*				const mc = StyleToClass(desktopStyles, true, item.uniqueId)*/}
+			{/*				const dc = StyleToClass(mobileStyles, false, item.uniqueId)*/}
+			{/*				const gc = StyleToClass(globalStyles, false, item.uniqueId)*/}
+			{/*				return gc + dc + mc + `.${item.uniqueId}-content{*/}
+			{/*	align-items:${mobileAlignItems};*/}
+			{/*	justify-content:${mobileJustifyContent};*/}
+			{/*	display:${mobileDisplay};*/}
+			{/*	}*/}
+			{/*	@media only screen and (min-width: 840px) {*/}
+			{/*	.${item.uniqueId}-content{*/}
+			{/*	align-items:${desktopAlignItems};*/}
+			{/*	justify-content:${desktopJustifyContent};*/}
+			{/*	display:${desktopDisplay};*/}
+			{/*	}*/}
+			{/*	}`*/}
+			{/*			}*/}
+			{/*		}).join("")}*/}
+			{/*	*/}
+            {/*    `}*/}
+			{/*	</style>*/}
 
-			{data.content !== null && JSON.parse(data.content).map((item, index) =>
-				<WebComponentGenerator  key={index} item={item}/>
-			)}
+			{/*{data.content !== null && JSON.parse(data.content).map((item, index) =>*/}
+			{/*	<WebComponentGenerator  key={index} item={item}/>*/}
+			{/*)}*/}
 		</div>
 	)
 }
