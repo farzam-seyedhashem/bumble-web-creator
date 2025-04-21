c7a215d0a0bb254ef17d4ffc5897c5d20f5042b1
farzamseyedhashem <farzam.seyedhashem@gmail.com>
2025-04-21 22:20:22 +0330

    fwefew

--- src/app/(frontend)/(website)/layout.js
+++ src/app/(frontend)/(website)/layout.js
@@ -1,43 +1,37 @@
-// import DesktopMenu from "@website/DesktopMenu";
-import MobileMenu from "@admin/admin-panel/menu/Mobile";
-import {revalidateTag} from "next/cache";
-import {notFound} from "next/navigation";
-import Icon from "@m3/assets/icons/Icon";
-import IconButton from "@m3/icon_buttons/IconButton";
-import WebsiteDesktopMenu from "@website/menu/WebsiteDesktopMenu";
-import {rgbaObjToRgba} from "@/_helper/rgbaObjtoRgba";
-import WebsiteMobileMenu from "@website/menu/WebsiteMobileMenu";
-import WebsiteFooter from "@website/WebsiteFooter";
-import {getMenu} from "@controller/MenuController";
-import {getFooter} from "@controller/FooterController";
-import {getSiteSetting} from "@controller/SiteSettingController";
+// import WebsiteDesktopMenu from "@website/menu/WebsiteDesktopMenu";
+// import {rgbaObjToRgba} from "@/_helper/rgbaObjtoRgba";
+// import WebsiteMobileMenu from "@website/menu/WebsiteMobileMenu";
+// import WebsiteFooter from "@website/WebsiteFooter";
+// import {getMenu} from "@controller/MenuController";
+// import {getFooter} from "@controller/FooterController";
+// import {getSiteSetting} from "@controller/SiteSettingController";
 
 
 
 export default async function layout({children}) {
-    const siteSetting = JSON.parse(await getSiteSetting());
-    const menuSetting = await getMenu()
-    const footerSetting = await getFooter()
+    // const siteSetting = JSON.parse(await getSiteSetting());
+    // const menuSetting = await getMenu()
+    // const footerSetting = await getFooter()
     return (
         <>
-            <style>
-                {`
-                .page-style{
-                background:${rgbaObjToRgba(siteSetting.color.background)};
-                }
-                `}
-            </style>
-        <div className={" page-style min-h-screen"}>
+        {/*    <style>*/}
+        {/*        {`*/}
+        {/*        .page-style{*/}
+        {/*        background:${rgbaObjToRgba(siteSetting.color.background)};*/}
+        {/*        }*/}
+        {/*        `}*/}
+        {/*    </style>*/}
+        {/*<div className={" page-style min-h-screen"}>*/}
 
-            <WebsiteDesktopMenu menuSetting={menuSetting} siteSetting={siteSetting}/>
+        {/*    <WebsiteDesktopMenu menuSetting={menuSetting} siteSetting={siteSetting}/>*/}
 
-            <WebsiteMobileMenu menuSetting={menuSetting} siteSetting={siteSetting}/>
-            <div className={"min-h-screen md:pt-[64px] md:pb-0 pb-[80px] pt-[64px]"}>
-                {children}
-            </div>
-            <WebsiteFooter footerSetting={JSON.stringify(footerSetting)} siteSetting={siteSetting}/>
+        {/*    <WebsiteMobileMenu menuSetting={menuSetting} siteSetting={siteSetting}/>*/}
+        {/*    <div className={"min-h-screen md:pt-[64px] md:pb-0 pb-[80px] pt-[64px]"}>*/}
+        {/*        {children}*/}
+        {/*    </div>*/}
+        {/*    <WebsiteFooter footerSetting={JSON.stringify(footerSetting)} siteSetting={siteSetting}/>*/}
 
-        </div>
+        {/*</div>*/}
         </>
     )
 }
