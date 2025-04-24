971c294fc79e6055e3594f5c336402cd231977b4
farzamseyedhashem <farzam.seyedhashem@gmail.com>
2025-04-24 20:07:35 +0330

    fwefew

--- src/components/website/component/LottieFile.js
+++ src/components/website/component/LottieFile.js
@@ -3,9 +3,13 @@
 // const Lottie = dynamic(() => import('react-lottie'), { ssr: false });
 
 // const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
+import dynamic from "next/dynamic";
+// import Lottie from "lottie-react";
 
+const DynamicLottie = dynamic(() => import("lottie-react"), { ssr: false });
 // import Lottie from "react-lottie";
-import { useLottie } from 'lottie-react'
+// import { useLottie } from 'lottie-react'
+// const {useLottie} = DynamicLottie
 
 export default function LottieFile({
 
@@ -17,26 +21,19 @@ export default function LottieFile({
 		autoplay: true,
 		animationData: JSON.parse(item.value),
 	}
-	const { View } = useLottie(options)
+	// const { View } = useLottie(options)
 	return (
 		<>
-			<div className="w-full h-fit flex items-center justify-center text-center">
-				<div className="lottie">{View}</div>
-			</div>
-			{/*{item?.value && <Lottie*/}
-			{/*	options={{*/}
-
-			{/*		loop: true,*/}
-			{/*		autoplay: true,*/}
-			{/*		animationData: JSON.parse(item.value),*/}
-			{/*		rendererSettings: {*/}
-			{/*			preserveAspectRatio: "xMidYMid slice"*/}
-			{/*		}*/}
-
-			{/*	}}*/}
-			{/*	height={400}*/}
-			{/*	width={400}*/}
-			{/*/>}*/}
+			{/*<div className="w-full h-fit flex items-center justify-center text-center">*/}
+			{/*	<div className="lottie">{View}</div>*/}
+			{/*</div>*/}
+			{item?.value && <DynamicLottie
+				className={"w-full h-[400px]"}
+				loop={true}
+				autoplay={true}
+				height={400}
+				width={400}
+			 animationData={JSON.parse(item.value)}/>}
 		</>
 	)
 }
