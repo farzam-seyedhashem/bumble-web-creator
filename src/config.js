454ddb528ba9bda15278fcd062622aff41b421ec
farzamseyedhashem <farzam.seyedhashem@gmail.com>
2025-04-24 20:53:59 +0330

    fwefew

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	ApiURL: "http://45.79.218.197:3001/api",
+	ApiURL: "http://45.79.218.197:3000/api",
 	FileUploadApiURL: "http://45.79.218.197:3001/upload",
 	FileUploadStorageURL: "http://45.79.218.197:3001/uploaded/"
 }
